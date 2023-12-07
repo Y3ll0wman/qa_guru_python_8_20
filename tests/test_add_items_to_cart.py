fb937c786f0570a93fca31d7b146b550e4914c11
Konstantin Varvarkin <varvarkin.k@gmail.com>
2023-12-08 01:30:40 +0300

    Added allure steps

--- tests/test_add_items_to_cart.py
+++ tests/test_add_items_to_cart.py
@@ -1,4 +1,6 @@
 import requests
+from allure import step
+
 from selene import browser, be, have
 
 
@@ -10,17 +12,19 @@ class TestAddItemsToCart:
     order_total = browser.element('.product-price.order-total')
 
     def go_to_cart(self):
-        self.shopping_cart.should(be.clickable).click()
+        with step('Open shopping cart'):
+            self.shopping_cart.should(be.clickable).click()
 
     @staticmethod
     def open_browser_with_added_item(url=None, data=None):
-        results = requests.post(url=url,
-                                data=data)
+        with step('Open browser with added item'):
+            results = requests.post(url=url,
+                                    data=data)
 
-        cookies = results.cookies.get('Nop.customer')
+            cookies = results.cookies.get('Nop.customer')
 
-        browser.open('/')
-        browser.driver.add_cookie({'name': 'Nop.customer', 'value': cookies})
+            browser.open('/')
+            browser.driver.add_cookie({'name': 'Nop.customer', 'value': cookies})
 
     def test_add_build_your_own_expensive_computer(self, api_url, browser_chrome):
         # GIVEN
@@ -35,13 +39,14 @@ class TestAddItemsToCart:
         self.go_to_cart()
 
         # THEN
-        self.product_details.should(have.text('Build your own expensive computer'))
-        self.product_details.should(have.text('Processor: Medium [+15.00]'))
-        self.product_details.should(have.text('RAM: 2 GB'))
-        self.product_details.should(have.text('HDD: 320 GB'))
-        self.unit_price.should(have.text('1815.00'))
-        self.product_subtotal.should(have.text('1815.00'))
-        self.order_total.should(have.text('1815.00'))
+        with step('product details check'):
+            self.product_details.should(have.text('Build your own expensive computer'))
+            self.product_details.should(have.text('Processor: Medium [+15.00]'))
+            self.product_details.should(have.text('RAM: 2 GB'))
+            self.product_details.should(have.text('HDD: 320 GB'))
+            self.unit_price.should(have.text('1815.00'))
+            self.product_subtotal.should(have.text('1815.00'))
+            self.order_total.should(have.text('1815.00'))
 
     def test_add_14_1_inch_laptop(self, api_url, browser_chrome):
         # GIVEN
@@ -52,10 +57,11 @@ class TestAddItemsToCart:
         self.go_to_cart()
 
         # THEN
-        self.product_details.should(have.text('14.1-inch Laptop'))
-        self.unit_price.should(have.text('1590.00'))
-        self.product_subtotal.should(have.text('1590.00'))
-        self.order_total.should(have.text('1590.00'))
+        with step('product details check'):
+            self.product_details.should(have.text('14.1-inch Laptop'))
+            self.unit_price.should(have.text('1590.00'))
+            self.product_subtotal.should(have.text('1590.00'))
+            self.order_total.should(have.text('1590.00'))
 
     def test_add_computing_and_internet_book(self, api_url, browser_chrome):
         # GIVEN
@@ -66,10 +72,11 @@ class TestAddItemsToCart:
         self.go_to_cart()
 
         # THEN
-        self.product_details.should(have.text('Computing and Internet'))
-        self.unit_price.should(have.text('10.00'))
-        self.product_subtotal.should(have.text('10.00'))
-        self.order_total.should(have.text('10.00'))
+        with step('product details check'):
+            self.product_details.should(have.text('Computing and Internet'))
+            self.unit_price.should(have.text('10.00'))
+            self.product_subtotal.should(have.text('10.00'))
+            self.order_total.should(have.text('10.00'))
 
     def test_add_digital_slr_camera_12_2_mpixel(self, api_url, browser_chrome):
         # GIVEN
@@ -80,10 +87,11 @@ class TestAddItemsToCart:
         self.go_to_cart()
 
         # THEN
-        self.product_details.should(have.text('Digital SLR Camera - Black'))
-        self.unit_price.should(have.text('670.00'))
-        self.product_subtotal.should(have.text('670.00'))
-        self.order_total.should(have.text('670.00'))
+        with step('product details check'):
+            self.product_details.should(have.text('Digital SLR Camera - Black'))
+            self.unit_price.should(have.text('670.00'))
+            self.product_subtotal.should(have.text('670.00'))
+            self.order_total.should(have.text('670.00'))
 
     def test_add_black_and_white_diamond_heart(self, api_url, browser_chrome):
         # GIVEN
@@ -94,7 +102,8 @@ class TestAddItemsToCart:
         self.go_to_cart()
 
         # THEN
-        self.product_details.should(have.text('Black & White Diamond Heart'))
-        self.unit_price.should(have.text('130.00'))
-        self.product_subtotal.should(have.text('130.00'))
-        self.order_total.should(have.text('130.00'))
+        with step('product details check'):
+            self.product_details.should(have.text('Black & White Diamond Heart'))
+            self.unit_price.should(have.text('130.00'))
+            self.product_subtotal.should(have.text('130.00'))
+            self.order_total.should(have.text('130.00'))
