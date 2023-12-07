dcccfae6b8df4ad2ff34005f89c8f8b487e9a980
Konstantin Varvarkin <varvarkin.k@gmail.com>
2023-12-08 01:55:20 +0300

    Added allure attach

--- tests/conftest.py
+++ tests/conftest.py
@@ -15,14 +15,16 @@ def browser_chrome(request):
 
     Настраивает окно браузера с заданными размерами, запускает браузер в headless режиме.
     """
+    options = webdriver.ChromeOptions()
+    options.add_argument('--headless')
+
+    browser.config.driver_options = options
+
+    config.base_url = 'https://demowebshop.tricentis.com'
     config.window_width = 1920
     config.window_height = 1080
     config.reports_folder = './.reports'
-    config.base_url = 'https://demowebshop.tricentis.com'
     config.timeout = 5
-    options = webdriver.ChromeOptions()
-    options.browser_version = '118.0'
-    options.add_argument('--no-sandbox')
 
     yield
 
--- tests/test_add_items_to_cart.py
+++ tests/test_add_items_to_cart.py
@@ -1,5 +1,6 @@
 import requests
-from allure import step
+from allure import step, attach
+from allure_commons.types import AttachmentType
 
 from selene import browser, be, have
 
@@ -22,6 +23,7 @@ class TestAddItemsToCart:
                                     data=data)
 
             cookies = results.cookies.get('Nop.customer')
+            attach(body=cookies, name='cookies', attachment_type=AttachmentType.TEXT)
 
             browser.open('/')
             browser.driver.add_cookie({'name': 'Nop.customer', 'value': cookies})
